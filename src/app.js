787b95e3c1ca83e656f92ffe55ef516b308a0219
pannevi <Vinothkumar.Pannerselvam@tecnotree.com>
2025-04-26 19:55:09 +0530

    user post patch delete method example

--- src/app.js
+++ src/app.js
@@ -3,18 +3,54 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 
 const app = express();
+app.use(express.json());
+
 
 app.post("/signup", async (req, res) => {
-    let user = new User({
-        //  firstName: "Vinothkumar",
-        lastName: "P",
-        emailId: "pvinothkumar93@gmail.com",
-        phoneNumber: "9884548985"
-    });
-    await user.save();
-    res.send("Data Saved Successfuly to signup collection")
+    try {
+        let user = new User(req.body);
+        await user.save();
+        res.send("Data Saved Successfuly to signup collection");
+    }
+    catch (err) {
+        res.status(400).send(err.message)
+    }
 });
 
+app.get("/user", async (req, res) => {
+    try {
+        let users = await User.find({});
+        res.send(users);
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong!")
+    }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+    try {
+        let userId = req.params.userId;
+        let body = req.body;
+        console.log(userId)
+        console.log(body)
+        await User.findByIdAndUpdate({ _id: userId }, body);
+        res.send("Updated Succesfully");
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong!")
+    }
+});
+
+app.delete("/user", async (req, res) => {
+    try {
+        let userId = req.body.userId;
+        await User.findByIdAndDelete({ _id: userId });
+        res.send("User Deleted Successfully!");
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong!")
+    }
+});
 
 
 connectDB().then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -2,21 +2,45 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String,
-        require:true
+        type: String
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        trim:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:true
     },
     phoneNumber: {
         type: String
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if(!["male","female","others"].includes(value)) {
+                throw new Error("Gender is not valid")
+            }
+        }
+    },
+    age :{
+        type:Number,
+        min:18
+    },
+    photoURL:{
+        type:String
+    },
+    skills:{
+        type:[String]
     }
+},{
+    timestamps:true
 });
 
 const User = mongoose.model("User", userSchema);
